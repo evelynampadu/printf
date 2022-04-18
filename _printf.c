8400f4f2d304a488473ce29423590442d5d77b52
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-04-18 19:13:30 +0000

    printf.c file

--- _printf.c
+++ _printf.c
@@ -1,49 +1,47 @@
 #include "main.h"
 
 /**
- *_printf - Print a formatted string
- *@format: format string
- *Return: number of characters printed
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list list;
-	char *pointer, *start;
-	param_func flags = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	va_start(list, format);
+	register int count = 0;
 
+	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (pointer = (char *)format; *pointer; pointer++)
+	for (p = format; *p; p++)
 	{
-		init_params(&flags, list);
-		if (*pointer != '%')
+		if (*p == '%')
 		{
-			count += _putchar(*pointer);
-			continue;
-		}
-		start = pointer;
-		pointer++;
-		while (get_flags(pointer, &flags))
-		{
-			pointer++;
-		}
-		pointer = get_width(pointer, &flags, list);
-		pointer = get_precision(pointer, &flags, list);
-		if (get_mods(pointer, &flags))
-			pointer++;
-
-		if (!func_parse(pointer))
-			count += print_range(start, pointer,
-			flags.l_mod || flags.h_mod ? pointer - 1 : 0);
-		else
-			count += print_func(pointer, list, &flags);
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
 	_putchar(-1);
-	va_end(list);
+	va_end(arguments);
 	return (count);
 }
